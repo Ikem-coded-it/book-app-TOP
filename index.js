895dc8318688fa3db8ecc931e1fcb9d0fda05fde
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-02-21 08:22:54 +0100

    finished add new book function

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let library = [
-    {title: "book1", author: "author1", pages: 55, read: "read"},
-    {title: "book2", author: "author2", pages: 54, read: "not read"},
-    {title: "book3", author: "author3", pages: 200, read: "not read"},
-    {title: "book4", author: "author4", pages: 621, read: "read"},
+    {title: "One piece", author: "Eiichiro Oda", pages: 10000, read: "read"},
+    {title: "Naruto", author: "Masashi Kishimoto", pages: 2000, read: "not read"},
+    {title: "Avatar: Last Air Bender", author: "Michael Dante", pages: 200, read: "not read"},
 ]
 
 function Book(title, author, pages, read) {
@@ -12,10 +11,29 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-let libraryContainer = document.querySelector('div');
+function getInput() {
+    const form = document.querySelector("#form");
+    form.addEventListener("submit", function(e) {
+        e.preventDefault()
+        let title = e.target.title.value;
+        let author = e.target.author.value;
+        let pages = e.target.pages.value;
+
+        let read = e.target.read.checked;
+        if (read != true) {
+            e.target.read.value = "not read"
+        }
+        
+        let readValue = e.target.read.value;
+        const newBook = new Book(title, author, pages, readValue)
+        library.push(newBook)
+        appendNewBook(library[library.length - 1])
+    })
+}
+
+let libraryContainer = document.querySelector('.library-container');
 
 function displayBook() {
-    console.log(libraryContainer)
     library.forEach(book => {
         console.log(book)
         const bookContainer = document.createElement("div")
@@ -37,4 +55,24 @@ function displayBook() {
     })
 }
 
+function appendNewBook(book) {
+    const bookContainer = document.createElement("div")
+    const title = document.createElement("h3")
+    const author = document.createElement("p")
+    const pages = document.createElement("div")
+    const readToggleButton = document.createElement("button")
+    
+    title.innerText = book.title;
+    author.innerText = book.author;
+    pages.innerText = book.pages;
+    readToggleButton.innerText = book.read
+        
+    bookContainer.appendChild(title)
+    bookContainer.appendChild(author)
+    bookContainer.appendChild(pages)
+    bookContainer.appendChild(readToggleButton)
+    libraryContainer.appendChild(bookContainer);
+}
+
+getInput()
 displayBook()
