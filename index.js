a5d32c2f0975c1fcfab8c1f9a8690d4f6fe5369f
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2023-03-04 18:59:24 +0100

    refactored to use clsses

--- index.js
+++ index.js
@@ -1,26 +1,78 @@
 let library = []
 
-// Book constructor
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
+class Book {
 
-Book.prototype.changeStatus = function(button) {
-    if (this.read != "Read" && button.innerText != "Read") {
-        this.read = "Read"
-        button.innerText = "Read"
-        button.classList.remove("read-btn-notread")
-        button.classList.toggle("read-btn-read")
-        return
-    }  
+    constructor (title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
+    
+    changeStatus (button) {
+        if (this.read != "Read" && button.innerText != "Read") {
+            this.read = "Read"
+            button.innerText = "Read"
+            button.classList.remove("read-btn-notread")
+            button.classList.toggle("read-btn-read")
+            return
+        } 
 
-    this.read = "Not read"
-    button.innerText = "Not read"
-    button.classList.remove("read-btn-read")
-    button.classList.toggle("read-btn-notread")
+        this.read = "Not read"
+        button.innerText = "Not read"
+        button.classList.remove("read-btn-read")
+        button.classList.toggle("read-btn-notread")
+    }
+
+    deleteBook(button) {
+        library.splice(library.indexOf(this), 1)
+        const bookCard = button.parentElement.parentElement;
+        bookCard.remove();
+    }
+
+    appendBookCard() {
+        const bookContainer = document.createElement("div")
+        bookContainer.classList.add("book-card")
+        const title = document.createElement("h3")
+        const author = document.createElement("p")
+        const pages = document.createElement("div")
+
+        const readToggleButton = document.createElement("button")
+        readToggleButton.classList.add("read-btn")
+        if (this.read == "Read") {
+            readToggleButton.classList.add("read-btn-read")
+        } else if (this.read == "Not read")  {
+            readToggleButton.classList.add("read-btn-notread")
+        }
+        readToggleButton.dataset.libraryIndex = library.indexOf(this)
+        readToggleButton.addEventListener("click", (e) => {
+            this.changeStatus(readToggleButton)
+        })
+
+        const deleteButton = document.createElement("button")
+        deleteButton.classList.add("delete-btn")
+        deleteButton.dataset.libraryIndex = library.indexOf(this)
+        deleteButton.addEventListener("click", (e) => {
+            this.deleteBook(deleteButton)
+        })
+
+        const bookBtnContainer = document.createElement("div");
+        bookBtnContainer.classList.add("book-btn-container")
+        bookBtnContainer.appendChild(readToggleButton)
+        bookBtnContainer.appendChild(deleteButton)
+        
+        title.innerText = this.title;
+        author.innerText = 'by' + ' ' + this.author;
+        pages.innerText = this.pages + ' ' + 'pages';
+        readToggleButton.innerText = this.read
+        deleteButton.innerText = "Delete"
+            
+        bookContainer.appendChild(title)
+        bookContainer.appendChild(author)
+        bookContainer.appendChild(pages)
+        bookContainer.appendChild(bookBtnContainer)
+        libraryContainer.appendChild(bookContainer);
+    }
 }
 
  // Create default library books
@@ -28,51 +80,7 @@ library.push(new Book("One piece", "Eiichiro Oda", 10000, "Read"))
 library.push(new Book("Naruto", "Masashi Kishimoto", 2000, "Not read"))
 library.push(new Book("Avatar: Last Air Bender", "Michael Dante", 200, "Read"))
 
-// Create and append elements for single book
-function appendBookCard(book) {
-    const bookContainer = document.createElement("div")
-    bookContainer.classList.add("book-card")
-    const title = document.createElement("h3")
-    const author = document.createElement("p")
-    const pages = document.createElement("div")
-
-    const readToggleButton = document.createElement("button")
-    readToggleButton.classList.add("read-btn")
-    if (book.read == "Read") {
-        readToggleButton.classList.add("read-btn-read")
-    } else if (book.read == "Not read")  {
-        readToggleButton.classList.add("read-btn-notread")
-    }
-    readToggleButton.dataset.libraryIndex = library.indexOf(book)
-    readToggleButton.addEventListener("click", (e) => {
-        book.changeStatus(readToggleButton)
-    })
-
-    const deleteButton = document.createElement("button")
-    deleteButton.classList.add("delete-btn")
-    deleteButton.dataset.libraryIndex = library.indexOf(book)
-    deleteButton.addEventListener("click", (e) => {
-        deleteBook(e.target.dataset.libraryIndex, deleteButton)
-    })
-
-    const bookBtnContainer = document.createElement("div");
-    bookBtnContainer.classList.add("book-btn-container")
-    bookBtnContainer.appendChild(readToggleButton)
-    bookBtnContainer.appendChild(deleteButton)
-    
-    title.innerText = book.title;
-    author.innerText = 'by' + ' ' + book.author;
-    pages.innerText = book.pages + ' ' + 'pages';
-    readToggleButton.innerText = book.read
-    deleteButton.innerText = "Delete"
-        
-    bookContainer.appendChild(title)
-    bookContainer.appendChild(author)
-    bookContainer.appendChild(pages)
-    bookContainer.appendChild(bookBtnContainer)
-    libraryContainer.appendChild(bookContainer);
-}
-
+ 
 
 let libraryContainer = document.querySelector('.library-container');
 let form = document.querySelector(".form");
@@ -95,14 +103,14 @@ function getInput() {
         let readValue = e.target.read.value;
         const newBook = new Book(title, author, pages, readValue)
         library.push(newBook)
-        appendBookCard(library[library.length - 1])
+        newBook.appendBookCard()
         hideForm()
     })
 }
 
 function displayBook() {
     library.forEach(book => {
-        appendBookCard(book)
+        book.appendBookCard()
     })
 }
 
@@ -117,12 +125,6 @@ function hideForm() {
     })
     form.classList.toggle("show") 
     newBookBtn.classList.toggle("show")
-}
-
-function deleteBook(index, button) {
-    library.splice(index, 1)
-    const bookCard = button.parentElement.parentElement;
-    bookCard.remove();
 }
 
 getInput()
